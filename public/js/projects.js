76c95ddfcb6c9769fdaff8321bb44e05b907d29d
Damián Silvani <munshkr@gmail.com>
2013-08-31 20:06:14 -0300

    Use awesome jQuery File Upload widget for uploading docs

---
+++ public/js/projects.js
@@ -0,0 +1,30 @@
+/*jslint nomen: true, regexp: true */
+/*global $, window, blueimp */
+
+$(function () {
+  'use strict';
+
+  // Initialize the jQuery File Upload widget:
+  $('#fileupload').fileupload({
+    // Uncomment the following to send cross-domain cookies:
+    //xhrFields: {withCredentials: true},
+    url: '/projects/new',
+    //maxFileSize: 5000000,
+    acceptFileTypes: /(\.|\/)(pdf|doc|xls)$/i
+  });
+
+  // Load existing files:
+  $('#fileupload').addClass('fileupload-processing');
+  $.ajax({
+    // Uncomment the following to send cross-domain cookies:
+    //xhrFields: {withCredentials: true},
+    url: $('#fileupload').fileupload('option', 'url'),
+    dataType: 'json',
+    context: $('#fileupload')[0]
+  }).always(function () {
+    $(this).removeClass('fileupload-processing');
+  }).done(function (result) {
+    $(this).fileupload('option', 'done')
+    .call(this, null, {result: result});
+  });
+});
