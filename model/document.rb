035e6f960bc280925d14a511b8c58e824d4269da
Damián Silvani <munshkr@gmail.com>
2013-08-31 22:26:12 -0300

    Add model validations

--- model/document.rb
+++ model/document.rb
@@ -1,17 +1,25 @@
 class Document < Sequel::Model
   plugin :timestamps
   plugin :schema
+  plugin :validation_helpers
 
   set_schema do
     primary_key :id
-    String      :filename, :null => false
-    String      :size, :null => false
+    String      :filename, null: false
+    String      :size, null: false
     String      :title
-    DateTime    :created_at, :null => false
+    DateTime    :created_at, null: false
     DateTime    :updated_at
     DateTime    :analyzed_at
     String      :state
-    Integer     :percentage
+    Integer     :percentage, null: false, default: 0
+  end
+
+  def validate
+    super
+    validates_presence [:filename, :size]
+    validates_unique :filename
+    validates_format /^\d+$/, [:size, :percentage]
   end
 
   def process!
--- model/project.rb
+++ model/project.rb
@@ -1,11 +1,17 @@
 class Project < Sequel::Model
   plugin :timestamps
   plugin :schema
+  plugin :validation_helpers
 
   set_schema do
     primary_key :id
     String      :name, :null => false
     DateTime    :created_at, :null => false
     DateTime    :updated_at
+  end
+
+  def validate
+    super
+    validates_presence :name
   end
 end
