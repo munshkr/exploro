c7334403f6afd058d63fb25bc38df26ea253d1ce
Damián Silvani <munshkr@gmail.com>
2013-09-07 14:06:57 -0300

    Store CSVs of entities per document

--- lib/jobs/entities_recognition_job.rb
+++ lib/jobs/entities_recognition_job.rb
@@ -12,57 +12,70 @@ class EntitiesRecognitionJob < DocumentJob
 
     doc.update(state: :entities_recognition)
 
-    logger.info "Extract named entities from content"
-    #doc.named_entities_dataset.destroy
-    doc_iter = doc.iterator
+    require 'csv'
+    csv_path = File.join(doc.project.path, 'entities', "#{doc.id}.csv")
+    FileUtils.mkdir_p(File.dirname(csv_path))
+    CSV.open(csv_path, 'wb') do |csv|
+      first_row = true
 
-    require 'lib/analyzer'
-    Analyzer.extract_named_entities(doc.processed_text).each do |ne_attrs, cur_pos, total_size|
-      inner_pos = {}
+      logger.info "Extract named entities from content"
+      #doc.named_entities_dataset.destroy
+      doc_iter = doc.iterator
 
-      doc_iter.seek(ne_attrs[:pos])
-      inner_pos["from"] = {
-        "pid" => doc_iter.page.id,
-        "tlid" => doc_iter.text_line.id,
-        "pos" => doc_iter.inner_pos,
-      }
+      require 'lib/analyzer'
+      Analyzer.extract_named_entities(doc.processed_text).each do |ne_attrs, cur_pos, total_size|
+        inner_pos = {}
 
-      last_token = (ne_attrs[:tokens] && ne_attrs[:tokens].last) || ne_attrs
-      doc_iter.seek(last_token[:pos] + last_token[:form].size - 1)
-      inner_pos["to"] = {
-        "pid" => doc_iter.page.id,
-        "tlid" => doc_iter.text_line.id,
-        "pos" => doc_iter.inner_pos,
-      }
+        doc_iter.seek(ne_attrs[:pos])
+        inner_pos["from"] = {
+          "pid" => doc_iter.page.id,
+          "tlid" => doc_iter.text_line.id,
+          "pos" => doc_iter.inner_pos,
+        }
 
-      ne_attrs[:inner_pos] = inner_pos
+        last_token = (ne_attrs[:tokens] && ne_attrs[:tokens].last) || ne_attrs
+        doc_iter.seek(last_token[:pos] + last_token[:form].size - 1)
+        inner_pos["to"] = {
+          "pid" => doc_iter.page.id,
+          "tlid" => doc_iter.text_line.id,
+          "pos" => doc_iter.inner_pos,
+        }
 
-      # TODO
-      # write CSV
+        ne_attrs[:inner_pos] = inner_pos
 
-      db << ne_attrs.merge(document_id: doc.id)
+        if first_row
+          csv << ne_attrs.keys
+          first_row = false
+        end
+        csv << ne_attrs.merge(
+          "tokens" => ne_attrs["tokens"].to_json,
+          "inner_pos" => ne_attrs["inner_pos"].to_json
+        ).values
 
-      if total_size && total_size > 0
-        doc.update(percentage: current_percentage(cur_pos, total_size))
-        #logger.info "Status #{doc.percentage} %"
+        if total_size && total_size > 0
+          doc.update(percentage: current_percentage(cur_pos, total_size))
+          #logger.info "Status #{doc.percentage} %"
+        end
+
+        #ne_klass = case ne_attrs[:ne_class]
+        #  when :addresses then AddressEntity
+        #  when :actions then ActionEntity
+        #  else NamedEntity
+        #end
+        #
+        #ne = ne_klass.new(ne_attrs)
+        #ne.save!
+
+        #doc.named_entities << ne
+        #doc.pages.in(:_id => ["from", "to"].map{ |k| ne.inner_pos[k]["pid"] }.uniq).each do |page|
+        #  page.named_entities << ne
+        #end
       end
 
-      #ne_klass = case ne_attrs[:ne_class]
-      #  when :addresses then AddressEntity
-      #  when :actions then ActionEntity
-      #  else NamedEntity
-      #end
-      #
-      #ne = ne_klass.new(ne_attrs)
-      #ne.save!
+    end # CSV.open
 
-      #doc.named_entities << ne
-      #doc.pages.in(:_id => ["from", "to"].map{ |k| ne.inner_pos[k]["pid"] }.uniq).each do |page|
-      #  page.named_entities << ne
-      #end
-    end
-
-    db.flush
+    #logger.info "Re-index project's Xapian DB"
+    #project.reindex
 
     #logger.info "Count classes of named entities found"
     #doc.information = {
@@ -84,24 +97,5 @@ class EntitiesRecognitionJob < DocumentJob
     #end
 
     next_job!(id)
-  end
-
-private
-  def self.db
-    require 'xapian-fu'
-
-    @@db ||= XapianFu::XapianDb.new(
-      dir: File.join(DB_PATH, 'entities'),
-      create: true,
-      fields: {
-        document_id: { type: Integer, store: false },
-        text:        { type: String,  store: true  },
-        pos:         { type: Fixnum,  store: true  },
-        ne_class:    { type: String,  store: true  },
-        form:        { type: String,  store: true  },
-        lemma:       { type: String,  store: true  },
-        tag:         { type: String,  store: true  },
-      }
-    )
   end
 end
